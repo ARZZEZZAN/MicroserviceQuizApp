4d845fac9140add06c14815f74c920b5f8972c72
killeral <arzzezzan@mail.ru>
2023-08-05 09:28:54 +0300

    HTTP CODE

--- QuizApp/src/main/java/com/arzezan/quizapp/Service/QuestionService.java
+++ QuizApp/src/main/java/com/arzezan/quizapp/Service/QuestionService.java
@@ -2,8 +2,11 @@ package com.arzezan.quizapp.Service;
 
 import com.arzezan.quizapp.DAO.QuestionDAO;
 import com.arzezan.quizapp.Entity.Question;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,16 +18,31 @@ public class QuestionService {
         this.questionDAO = questionDAO;
     }
 
-    public List<Question> getAllQuestions() {
-        return questionDAO.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDAO.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDAO.getQuestionsByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDAO.getQuestionsByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
-        questionDAO.save(question);
-        return "success";
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionDAO.save(question);
+            return new ResponseEntity<>("Success", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
     }
 }
