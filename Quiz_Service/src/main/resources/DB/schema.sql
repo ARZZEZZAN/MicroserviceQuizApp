d481fdd2b18faa3ec639ac50f8f3a4e28cd3c85a
killeral <arzzezzan@mail.ru>
2023-08-07 17:04:15 +0300

    API GATEWAY is ready

--- Quiz_Service/src/main/resources/DB/schema.sql
+++
@@ -1,8 +0,0 @@
-drop schema if exists Quiz cascade;
-create schema if not exists Quiz;
-
-
-CREATE TABLE Quiz.quiz (
-                               Id SERIAL PRIMARY KEY,
-                               title VARCHAR(10) NOT NULL
-);
